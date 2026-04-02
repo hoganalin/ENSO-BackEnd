ac0da342df1c3f34bcfbbaec7bd04c6f2ad632fa
hoganalin <hoganalin@gmail.com>
2026-04-02 11:27:03 +0800

    feat: update permissions in settings and adjust base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/vite-reacthomework-finalweek-backEnd/'
-      : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ENSO-BackEnd/' : '/',
   plugins: [react()],
 });
